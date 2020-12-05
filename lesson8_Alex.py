12ab79539781a4b426bbdab73d02a8e169892c18
sanyalav <sanyalav1983@gmail.com>
2020-12-05 10:35:53 +0200

    Ready home work 8

--- lesson8_Alex.py
+++ lesson8_Alex.py
@@ -3,7 +3,7 @@ import os
 from random import randint
 print("1)")
 def create_domains_list():
-    with open ("domains.txt", 'r') as file:
+    with open("domains.txt", 'r') as file:
         data = []
         my_list = []
         for line in file.readlines():
@@ -17,7 +17,7 @@ create_domains_list()
 # 2)
 print("2)")
 def create_surnames_list():
-    with open ("names.txt", 'r') as file:
+    with open("names.txt", 'r') as file:
         data_1 = []
         for line in file.readlines():
             data_1.append(line.split()[1])
@@ -27,22 +27,22 @@ create_surnames_list()
 # 3)
 print("3)")
 def create_email():
-    with open ("domains.txt", 'r') as file:
+    with open("domains.txt", 'r') as file:
         data = []
         for line in file.readlines():
             data.append(line.strip())
-    with open ("names.txt", 'r') as file:
+    with open("names.txt", 'r') as file:
         data_1 = []
         for line in file.readlines():
             data_1.append(line.split()[1])
     try:
         my_symbol = ""
-        rand_surname = randint(0,len(data_1))
-        rand_number = randint(100,999)
+        rand_surname = randint(0, len(data_1))
+        rand_number = randint(100, 999)
         for symbol in range(randint(5, 7)):
             rand_symbol = chr(randint(97, 122))
             my_symbol += rand_symbol
-        rand_domain = randint(0,len(data))
+        rand_domain = randint(0, len(data))
         print(f"{data_1[rand_surname]}.{rand_number}@{my_symbol}{data[rand_domain]}")
     except:
         pass
